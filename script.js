7203b1fb4ea926831548384329e10ceb13d434bb
bhuvanarao2003 <122141915+bhuvanarao2003@users.noreply.github.com>
2023-08-04 23:50:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 var names=new Array();
-names[0]="Yaakov";
-names[1]="John";
-names[2]="Jen";
-names[3]="jason";
-names[4]="paul";
-names[5]="frank";
-names[6]="larry";
-names[7]="paula";
-names[8]="laura";
-names[9]="jim";
+names[0]="oggy";
+names[1]="tom";
+names[2]="jerry";
+names[3]="shinchan";
+names[4]="doremon";
+names[5]="mr.bean";
+names[6]="pinkpanther";
+names[7]="pockemon";
+names[8]="hidy";
+names[9]="bheem";
 
 
 for (var i = 0; i < names.length; i++) {
